cf514f2ffcf99ffce15163698a471c448b62edec
Yathish <yathishhs680@gmail.com>
2025-11-17 21:55:58 +0530

    Changes

--- fibonacci.py
+++ fibonacci.py
@@ -2,11 +2,10 @@ nterms = int(input("How many terms? "))
 
 n1, n2 = 0, 1
 count = 0
-
-if nterms <= 0:
-   print("Please enter a positive integer")
+if nterms <=0:
+    print("Please enter a positive integer")
 elif nterms == 1:
-   print("Fibonacci sequence upto",nterms,":")
+
    print(n1)
 else:
    print("Fibonacci sequence:")
